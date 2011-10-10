41d132b2548c2f8f8ce0148b0b87afcc86928474
bend <ben.daccache@gmail.com>
2011-10-10 18:15:42 +0200

    finished tcp socket but not tested

---
+++ Include/Socket/Errors.h
@@ -0,0 +1,53 @@
+/**
+ *
+ *       @file  Errors.h
+ *
+ *      @brief  Defines errors 
+ *
+ *    @version  1.0
+ *       @date  10/10/2011 15:40:54
+ *
+ *     @author  Ben D. (BD), dbapps2@gmail.com
+ *
+ */
+
+/**
+ * Success
+ */
+static int SOSUCC = 1;
+
+/**
+ * Could not open Socket 
+ */
+static int SOCOPEN = -1;
+
+/**
+ * Host not found
+ */
+static int SONHOST = -2;
+
+/**
+ * Could not connect
+ */
+static int SOCONN = -3;
+
+/**
+ * Could not close the connection
+ */
+static int SOCLO  = -4;
+
+/**
+ * Could not write to socket
+ */
+static int SOEWRITE = -5;
+
+/**
+ * Could not read from socket
+ */
+static int SOEREAD = -6;
+
+/**
+ * Could not allocate mem
+ */
+static int SOEMEM = -7;
+
---
+++ Include/Socket/TcpSocket.h
@@ -0,0 +1,95 @@
+/**
+ *
+ *       @file  TcpSocket.h
+ *
+ *      @brief  C++ wrapper for the socket library
+ *
+ *    @version  1.0
+ *       @date  10/10/2011 14:47:02
+ *
+ *     @author  Ben D. (BD), dbapps2@gmail.com
+ *
+ */
+
+#ifndef TCP_SOCKET_H__
+#define TCP_SOCKET_H__
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <iostream>
+using namespace std;
+
+#include <Socket/Errors.h>
+
+class TcpSocket {
+public:
+	/**
+	 * @brief default constructor
+	 */
+	TcpSocket();
+
+	/**
+	 * @brief connects to the host on port
+	 * @param host : the host
+	 * @param the port 
+	 * @return SOSUCC if succeded 
+	 * 		   SOCOPEN if socket could not be opened
+	 * 		   SONHOST if host not found 
+	 * 		   SOCONN if could not connect to host
+	 * 		   
+	 */
+	int connect(string host, unsigned short port);
+
+	/**
+	 * @brief disconnects from the host
+	 * @return code
+	 */
+	int disconnect();
+	
+	/**
+	 * @brief sends a string over the net 
+	 */
+	int sendString(string &str);
+	
+	/**
+	 * @brief sends an int over the net 
+	 */
+	int sendInt(int &i);
+
+	/**
+	 * @brief sends a short over the net
+	 */
+	int sendShort(short &i);
+	
+	/**
+	 * @brief sends a char over the net 
+	 */
+	int sendChar(char &c);
+
+	/**
+	 * @brief sends a char array over the net
+	 */
+	int sendCharArray(char* c);
+
+	int receiveString(string &str);
+
+	int receiveInt(int &i);
+
+	int receiveShort(short& s);
+
+	int receiveChar(char &c);
+
+	int receiveCharArray(char **c);
+
+private:
+	int m_socketfd;
+	int m_portNo;
+	struct sockaddr_in m_servAddr;
+	struct hostent *m_server;
+
+};
+
+#endif
