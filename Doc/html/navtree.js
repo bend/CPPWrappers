a0098ad2bc85f4152cc20a68527b53f1ac5b9130
bend <ben.daccache@gmail.com>
2011-10-14 14:56:36 +0200

    added doxygen doc

---
+++ Doc/html/navtree.js
@@ -0,0 +1,291 @@
+var NAVTREE =
+[
+  [ "CPPWrappers", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "Logger", "class_logger.html", null ],
+      [ "Mutex", "class_mutex.html", null ],
+      [ "Semaphore", "class_semaphore.html", null ],
+      [ "TcpServer", "class_tcp_server.html", null ],
+      [ "TcpSocket", "class_tcp_socket.html", null ],
+      [ "Thread", "class_thread.html", null ],
+      [ "Time", "class_time.html", null ],
+      [ "Types", "class_types.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Members", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "Include/Socket/Errors.h", "_errors_8h.html", null ],
+      [ "Include/Socket/TcpServer.h", "_tcp_server_8h.html", null ],
+      [ "Include/Socket/TcpSocket.h", "_tcp_socket_8h.html", null ],
+      [ "Include/System/Logger.h", "_logger_8h.html", null ],
+      [ "Include/System/Mutex.h", "_mutex_8h.html", null ],
+      [ "Include/System/Semaphore.h", "_semaphore_8h.html", null ],
+      [ "Include/System/Thread.h", "_thread_8h.html", null ],
+      [ "Include/System/Time.h", "_time_8h.html", null ],
+      [ "Include/System/Types.h", "_types_8h.html", null ],
+      [ "Socket/TcpServer.cpp", "_tcp_server_8cpp.html", null ],
+      [ "Socket/TcpSocket.cpp", "_tcp_socket_8cpp.html", null ],
+      [ "System/Logger.cpp", "_logger_8cpp.html", null ],
+      [ "System/Mutex.cpp", "_mutex_8cpp.html", null ],
+      [ "System/Semaphore.cpp", "_semaphore_8cpp.html", null ],
+      [ "System/Thread.cpp", "_thread_8cpp.html", null ],
+      [ "System/Time.cpp", "_time_8cpp.html", null ],
+      [ "System/Types.cpp", "_types_8cpp.html", null ]
+    ] ],
+    [ "Directories", "dirs.html", [
+      [ "Include", "dir_8cb44e2b44697dfd5924d53a87316094.html", [
+        [ "Socket", "dir_f6a0c78662db84c2eb026d055762dee4.html", null ],
+        [ "System", "dir_4c78d9958f30f8c9738dea15631a37b5.html", null ]
+      ] ],
+      [ "Socket", "dir_8b9b8c7106f6280fe3a32311b68b19fb.html", null ],
+      [ "System", "dir_5b25e7294e083e37fc0840e783eb0df7.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
