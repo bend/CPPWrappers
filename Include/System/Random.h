09758ebab5546b3e6ab82677445c473fa73b09b7
bend <ben.daccache@gmail.com>
2011-10-19 00:56:30 +0200

    files created

---
+++ Include/System/Random.h
@@ -0,0 +1,14 @@
+/**
+ *
+ *       @file  Random.h
+ *
+ *      @brief  
+ *
+ *    @version  1.0
+ *       @date  10/19/2011 00:55:50
+ *
+ *     @author  Ben D. (BD), dbapps2@gmail.com
+ *
+ */
+
+
---
+++ System/Random.cpp
@@ -0,0 +1,14 @@
+/**
+ *
+ *       @file  Random.cpp
+ *
+ *      @brief  
+ *
+ *    @version  1.0
+ *       @date  10/19/2011 00:44:20
+ *
+ *     @author  Ben D. (BD), dbapps2@gmail.com
+ *
+ */
+
+
